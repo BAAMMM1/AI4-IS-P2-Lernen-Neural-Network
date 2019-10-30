b907380cd4c866879d8163f62a3cecbf03235e73
c.graumann <christian.graumann@haw-hamburg.de>
2019-10-30 14:10:21 +0100

    add view and controller for simple presentation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,43 @@
-public class Main {
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
-    public static void main(String[] args) {
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Paths;
 
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader();
+
+        URL path = new File(System.getProperty("user.dir") + "/src/main/java/view/sample.fxml").toURI().toURL();
+
+        Parent root = FXMLLoader.load(path);
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 550, 550));
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent e) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+
+        primaryStage.show();
     }
 
 
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
